260736537ff55e8db260d361ee4b783e3c0905ca
miguel <miguel@i7-2600k>
2018-02-12 23:36:36 +0100

    Añadido Serializalo

---
+++ src/dam_ed04_actividad/Serializalo.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dam_ed04_actividad;
+import java.io.Serializable;
+
+/**
+ *
+ * @author miguel
+ */
+public class Serializalo implements Serializable {
+    
+}
